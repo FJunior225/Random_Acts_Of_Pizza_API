4b00ab6ddbee283d1ebccf18d29ca46f486d6ee3
Noah Schutte <noahschutte@gmail.com>
2016-08-01 18:01:27 -0400

    add structure to route business logic

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,16 @@
 class RequestsController < ApplicationController
+
+  def create
+    request = Request.new(creator_id: User.find_by(params[:access_token]), title: params[:title], video: params[:video], pizzas: params[:pizzas], status: params[:status])
+    if round.save
+
+    else
+      render :status => :not_modified
+    end
+  end
+
+  def show
+    requests = Request.where(status: )
+  end
+
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,17 @@ class Request < ApplicationRecord
     validates_uniqueness_of :creator, :title
 
     belongs_to :creator, class_name: "User", foreign_key: :creator_id
+
+  def open_requests
+    self.requests.where("end_time > ?", DateTime.now)
+  end
+
+  def expired_requests
+    self.requests.where("end_time < ?", DateTime.now).reverse
+  end
+
+  def filled_requests
+    self.open_requests.find_all { |request| !request.has_photos?(self.id)}.reverse
+  end
+
 end

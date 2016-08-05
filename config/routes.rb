d6cd7f4ba090510fbf81d675c93fcbb7f3e44674
Noah Schutte <noahschutte@gmail.com>
2016-08-05 13:57:33 -0400

    change user state to hold authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :requests, only: [:create, :index, :show]
+  resources :requests, only: [:create, :index, :show, :update]
   resources :users, only: [:create, :show, :update]
-  
+
 end
--- db/migrate/20160729164640_create_users.rb
+++ db/migrate/20160729164640_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-        t.integer :access_token, null: false
+        t.integer :FB_userID, null: false
         t.string :first_name, null: false
         t.string :email
         t.integer :rating, default: 0

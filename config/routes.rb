63894e8bc04d0d1c54d78c05c418d6360d3bc9db
Noah Schutte <noahschutte@gmail.com>
2016-08-08 12:55:36 -0400

    accept current user state to persist requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
   resources :requests, only: [:create, :index, :show, :update]
-  resources :users, only: [:create, :index, :show, :update]
+  resources :users, only: [:create, :show, :update]
 end

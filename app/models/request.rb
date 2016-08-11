e86378c15a8b7e8e377b34c9468ae8ee48c8df0c
Noah Schutte <noahschutte@gmail.com>
2016-08-11 15:01:16 -0400

    remove 4 hour expiration on requests for demo purposes

--- app/models/request.rb
+++ app/models/request.rb
@@ -5,7 +5,8 @@ class Request < ApplicationRecord
     belongs_to :creator, class_name: "User", foreign_key: :creator_id
 
   def self.open_requests
-    Request.where("created_at > ?", DateTime.now - 4.hours).order('created_at ASC')
+    # DateTime.now - 4.hours
+    Request.where("created_at > ?", DateTime.now).order('created_at ASC')
   end
 
   def self.totalPizzasDonated
